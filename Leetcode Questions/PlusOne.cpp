57ab3577aa4ed51201de8e14a3ea38a449d6ef78
Nquartey17 <niikwarteiq@gmail.com>
2023-10-31 10:43:27 -0400

    PlusOne (incomplete)

---
+++ Leetcode Questions/PlusOne.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        vector<int> ans;
+        int num = 0;
+        for (int i = digits.size(); i >= 0; i--) {
+            num = num * 10 + digits[i];
+        }
+        num = num + 1;
+        int numCopy = num;
+        int length = 0;
+        while (numCopy != 0) {
+            int value = numCopy % 10;
+            numCopy = numCopy / 10;
+            ans.push_back(value);
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    vector<int> digits = {1,2,3};
+    Solution sol;
+}
